f811d4f46e609167743ca6ace4f6a8483b99233a
Vladimir Ivanov <51359590+IVladimirA@users.noreply.github.com>
2023-06-21 02:22:34 +0300

    feat: first pages (#1)

---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="destroyer@gmail.com">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Password</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="StrOngPasswOrd123">
+      </div>
+    </div>
+    
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Login</button>
+      </div>
+    </div>
+    </template>
